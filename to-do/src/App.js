99b05872e1be6b8118b3d32b8d2b026fe596c0a2
CaKlan <lhs74187@gmail.com>
2023-01-19 17:11:54 +0900

    삭제 시스템 복구

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -3,10 +3,12 @@ import React, {useState, useRef, useEffect} from 'react';
 
 function ListItem(props){
 
-  let textArea = useRef();
+  let textArea = useRef(); //ListItem의 
   let [context, setContext] = useState(props.text);
   let [isDisable, setIsDisable] = useState(false);
-  const checkHandler = e => {
+
+  
+  const checkHandler = e => { //체크박스 체크되면 text 취소선, 회색 아니면 그대로
     if (e.target.checked){
       textArea.current.setAttribute("style", "text-decoration:line-through;color:gray");
     }else{
@@ -19,37 +21,32 @@ function ListItem(props){
     setTimeout(()=>{textArea.current.focus()}, 1); // setTimeout 없이 focus가 안됨.
   }
 
-  const blurFunc = () => {
+  const blurFunc = () => { //수정 하다가 다른 곳 클릭했을때 메세지 저장
     let temp = [...props.toDoLists];
     temp[props.idx].msg = context;
+    console.log(temp);
     props.setToDoLists(temp);
 
-    console.log(props.toDoLists);
-
+    //console.log(props.toDoLists);
     setContext(textArea.current.value);
     modifyHandler();
   }
 
   const handleKeyUp = (e) => {
-    console.log(e.currentTarget);
+
     if (e.key === "Enter"){
       blurFunc();
       e.currentTarget.blur();
     }
   }
 
-  const doubleClick = (e) => {
-    
-    console.log(e.target);
-    
+  const doubleClick = (e) => { 
     modifyHandler();
-    // if(e.detail === 2){
-    //   modifyHandler();
-    //   if(e.target !== textArea.current){
-    //     textArea.current.focus();
-    //   }
-    // }
   }
+
+  useEffect(()=>{
+    console.log("context : " , setContext(props.text));
+  })
 
 //document.activeElement === textArea.current ? props.text : 'focus'
   return (
@@ -58,7 +55,7 @@ function ListItem(props){
         <input type="checkbox" onChange={checkHandler}/>
         <input className="listText" type="textarea" 
           value={context} ref={textArea} 
-          onBlur={blurFunc} 
+          onBlur={blurFunc}
           onChange={()=>{setContext(textArea.current.value);}}
           onKeyUp={handleKeyUp}
           disabled={isDisable ? false : true}/>
@@ -76,17 +73,17 @@ function ListItem(props){
 
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [toDoLists, setToDoLists] = useState([]);
-  const [itemId, setItemId] = useState(0);
+
+  const [message, setMessage] = useState(''); //할 일 입력 메세지 state
+  const [toDoLists, setToDoLists] = useState([]); //항목 리스트
+  const [itemId, setItemId] = useState(0); //항목 유니크 ID값
   const [total, setTotal] = useState([]);
 
-  const handleMessageChange = event => {
+  const handleMessageChange = event => { //message state에 저장되도록 함
     setMessage(event.target.value);
   };
 
-
-  const AddListItem = (e) => {
+  const AddListItem = (e) => { //항목 추가
     e.preventDefault();
 
     const item = {
@@ -106,7 +103,7 @@ function App() {
 
   };
 
-  const handleKeyUp = (e) => {
+  const handleKeyUp = (e) => { //Enter키 누르면 항목 추가
     if(e.key === 'Enter' && message != '') {
       AddListItem(e);
     }
@@ -122,6 +119,7 @@ function App() {
       
       />
     }, []));
+    console.log("total : ", toDoLists);
   }, [toDoLists]);
 
   const deleteItem = (idx, e) => {
@@ -130,10 +128,11 @@ function App() {
     let a = temp.filter((x)=>{
       return x.idx != idx;
     });
-
+    
     setToDoLists(a);
-    console.log("x : ", temp, " idx : ", idx);
+    //console.log("x : ", temp, " idx : ", idx);
   }
+  
   return (
     <div className="App">
     <div className="container">
